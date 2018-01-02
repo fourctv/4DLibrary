8a296f36904a1e8bf37499c2e5d08b00ef24ea7e
fourctv <julio@4ctv.com>
2018-01-02 18:43:22 -0200

    update to latest js44D

---
+++ src/app/studentList/studentSelector.modal.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'student-selector',
+    template: ''
+})
+
+export class StudentSelector {
+
+
+
+}
